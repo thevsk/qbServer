ee289a96f1215f531fe268dfcaa2560cdc0c71ce
mengr <zafkielcn@qq.com>
2018-07-10 18:27:22 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -3,37 +3,41 @@ const http = require('http');
 
 let config, host = '0.0.0.0', port = 7900, server;
 
-try {
-    config = fs.readFileSync(`${__dirname}/resources/config.json`);
-    config = JSON.parse(config);
-    host = config.host;
-    port = config.port;
-} catch (e) {
-    console.log(`read config.json fail`);
+let init = () => {
+    try {
+        config = fs.readFileSync(`${__dirname}/resources/config.json`);
+        config = JSON.parse(config);
+        host = config.host;
+        port = config.port;
+    } catch (e) {
+        console.log(`read config.json fail`);
+    }
+    
+    let onMessage = data => {
+        console.log(data);
+    }
+    
+    server = http.createServer();
+    
+    server.on('request', (req, res) => {
+        console.log(req.headers);
+        let data = [];
+        req.on('data', chunk => {
+            data.push(chunk);
+        });
+        req.on('end', () => {
+            onMessage(data.join(''));
+            res.statusCode = 204;
+            res.end();
+        });
+    });
+    
+    server.listen({
+        host: host,
+        port: port
+    }, () => {
+        console.log(`listen host => ${host}, port => ${port}`);
+    });
 }
 
-let onMessage = data => {
-    console.log(data);
-}
-
-server = http.createServer();
-
-server.on('request', (req, res) => {
-    console.log(req.headers);
-    let data = [];
-    req.on('data', chunk => {
-        data.push(chunk);
-    });
-    req.on('end', () => {
-        onMessage(data.join(''));
-        res.statusCode = 204;
-        res.end();
-    });
-});
-
-server.listen({
-    host: host,
-    port: port
-}, () => {
-    console.log(`listen host => ${host}, port => ${port}`);
-});
+init();
